4f0b8a4ad8f5f1ca4054a2803e701205493bbb5b
Hanasolo <hanasolo@hotmail.com>
2026-04-15 19:12:40 +0200

    creating basis html and download the resources

---
+++ src/assets/data.js
@@ -0,0 +1,50 @@
+export const data = [
+    {
+        image:"src/images/laptop_red_light.avif" ,
+        alt:"laptop in the darkness with red light on screem"
+    },
+    {
+        image:"src/images/building.avif" ,
+        alt:"building"
+    },
+    {
+        image:"src/images/code_image.avif" ,
+        alt:"code image"
+    },
+    {
+        image:"src/images/four_code_blocks.avif" ,
+        alt:"four code blocks"
+    },
+    {
+        image:"src/images/key_board.avif" ,
+        alt:"keyboard"
+    },
+    {
+        image:"src/images/laptop_and_window.avif" ,
+        alt:"laptop desk and window"
+    },
+    {
+        image:"src/images/love_to_learn_sign.avif" ,
+        alt:"a sign with love to learn written"
+    },
+    {
+        image:"src/images/plant.avif" ,
+        alt:"photo of a little green plant"
+    },
+    {
+        image:"src/images/stikers_on_the_table.avif" ,
+        alt:"lots of stickers on the table and a man touching them"
+    },
+    {
+        image:"src/images/watch_on_the_floor.avif" ,
+        alt:"a big watch painted on the floor and a man with a laptop in the middle"
+    },
+    {
+        image:"src/images/whiteboard_work.avif" ,
+        alt:"a whiteboard with stickers distributing the office workflow"
+    },
+    {
+        image:"src/images/work_space.avif" ,
+        alt:"a desk home "
+    },
+]
